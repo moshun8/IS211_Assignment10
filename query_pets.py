0ef044643f7512eabd065b14bf3992d14c6994b3
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2015-04-26 13:07:54 -0400

    query works now

--- query_pets.py
+++ query_pets.py
@@ -2,29 +2,63 @@
 # -*- coding: utf-8 -*-
 """query pets"""
 
-import sqlite3
+import sqlite3 as lite
 import sys
 
 con = None
 
 
 try:
-    con = sqlite3.connect('pets.db')
-    data = con.cursor()
+    con = lite.connect('pets.db')
+    con.row_factory = lite.Row
 
-    pickPerson = raw_input('Please select an ID number: ')
-    query = "SELECT * FROM person WHERE id =?"
+    while True:
+        pickPerson = raw_input('Please select an ID number: ')
 
-    if pickPerson == '-1':
-        sys.exit()
-    else:
-        for row in data.execute(query, [(pickPerson)]):
-            print row
-    # person = data.fetchone()
+        if pickPerson == '-1':
+            sys.exit()
+        else:
+            try:
+                pickPerson = int(pickPerson)
+            except:
+                print "Error, please input a number"
+                continue
 
-except sqlite3.Error as e:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM person WHERE id =?", [(pickPerson)])
+        row = cur.fetchone()
+
+        if row == None:
+            print 'Not a valid ID number. Pick again.'
+            continue
+
+        print row['first_name'] + ' ' + row['last_name'] + ' is ' + str(
+            row['age']) + ' years old.'
+
+
+        for row in con.execute(
+            "SELECT * FROM person_pet WHERE person_id =?", [(pickPerson)]):
+
+            for name in con.execute(
+                "SELECT * FROM person WHERE id =?", [(pickPerson)]):
+                owner = name['first_name'] + ' ' + name['last_name']
+
+            for petrow in con.execute(
+                "SELECT * FROM pet WHERE id =?", [(row['pet_id'])]):
+
+                if petrow['dead'] == 0:
+                    print ('- ' + owner + ' owns ' + petrow[
+                        'name'] + ', a ' + petrow['breed'] + ' who is ' + str(
+                            petrow['age']) + ' years old.')
+                else:
+                    print ('- ' + owner + ' owned ' + petrow[
+                        'name'] + ', a ' + petrow['breed'] + ' who was ' + str(
+                            petrow['age']) + ' years old.')
+
+
+except lite.Error as e:
     print "Closing."
-    print "Error {0}".format(e.args[0])
+    print "Error: %s " % e.args[0]
     sys.exit(1)
 
 finally:
